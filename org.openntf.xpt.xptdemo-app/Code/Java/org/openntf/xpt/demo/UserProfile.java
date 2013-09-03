c507024fb03ea710b8eb19bd113a778146cf570e
Christian Guedemann <christian.guedemann@webgate.biz>
2013-09-03 07:01:27 +0200

    Preparing BETA 1

--- org.openntf.xpt.xptdemo-app/Code/Java/org/openntf/xpt/demo/UserProfile.java
+++ org.openntf.xpt.xptdemo-app/Code/Java/org/openntf/xpt/demo/UserProfile.java
@@ -1,3 +1,18 @@
+/*
+ * © Copyright WebGate Consulting AG, 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
 package org.openntf.xpt.demo;
 
 import java.io.Serializable;
@@ -20,6 +35,8 @@ public class UserProfile implements Serializable {
 	private String m_Location;
 	@DominoEntity(FieldName="ZIP")
 	private String m_ZIP;
+	@DominoEntity(FieldName ="showWelcomeBox")
+	private boolean m_ShowWelcomeBox;
 	
 	public void setUserName(String userName) {
 		m_UserName = userName;
@@ -44,6 +61,12 @@ public class UserProfile implements Serializable {
 	}
 	public String getLocation() {
 		return m_Location;
+	}
+	public void setShowWelcomeBox(boolean showWelcomeBox) {
+		m_ShowWelcomeBox = showWelcomeBox;
+	}
+	public boolean isShowWelcomeBox() {
+		return m_ShowWelcomeBox;
 	}
 	
 }
