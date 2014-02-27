dee0f3e3641471960f968c6773d7c7163ba52804
Christian Guedemann <christian.guedemann@webgate.biz>
2014-02-27 16:12:10 +0100

    Profiling of processDocument

--- org.openntf.xpt.core/src/org/openntf/xpt/core/dss/binding/Domino2JavaBinder.java
+++ org.openntf.xpt.core/src/org/openntf/xpt/core/dss/binding/Domino2JavaBinder.java
@@ -19,39 +19,52 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
+import com.ibm.commons.util.profiler.Profiler;
+import com.ibm.commons.util.profiler.ProfilerAggregator;
+import com.ibm.commons.util.profiler.ProfilerType;
+
 import lotus.domino.Document;
 import lotus.domino.NotesException;
 
 public class Domino2JavaBinder {
 
 	private ArrayList<Definition> m_Definition;
+	private static final ProfilerType pt = new ProfilerType("XPT.DSS.Domion2JavaBinder");
 
 	public Domino2JavaBinder() {
 		m_Definition = new ArrayList<Definition>();
 	}
 
-	public void addDefinition(String strNotesField, String strJavaField,
-			IBinder<?> binCurrent, boolean changeLog, HashMap<String, Object> addValues, boolean encrypted, String[] encRoles) {
-		m_Definition.add(new Definition(strNotesField, strJavaField,
-				binCurrent,changeLog, addValues, encrypted, encRoles));
+	public void addDefinition(String strNotesField, String strJavaField, IBinder<?> binCurrent, boolean changeLog, HashMap<String, Object> addValues,
+			boolean encrypted, String[] encRoles) {
+		m_Definition.add(new Definition(strNotesField, strJavaField, binCurrent, changeLog, addValues, encrypted, encRoles));
 	}
 
-	public void processDocument(Document docProcess, Object objCurrent)
-			throws NotesException {
-		for (Iterator<Definition> itDefinition = m_Definition.iterator(); itDefinition
-				.hasNext();) {
+	public void processDocument(Document docProcess, Object objCurrent) throws NotesException {
+		if (Profiler.isEnabled()) {
+			ProfilerAggregator pa = Profiler.startProfileBlock(pt, "processDocument");
+			long startTime = Profiler.getCurrentTime();
+			try {
+				_processDocument(docProcess, objCurrent);
+			} finally {
+				Profiler.endProfileBlock(pa, startTime);
+			}
+		} else {
+			_processDocument(docProcess, objCurrent);
+		}
+
+	}
+
+	private void _processDocument(Document docProcess, Object objCurrent) throws NotesException {
+		for (Iterator<Definition> itDefinition = m_Definition.iterator(); itDefinition.hasNext();) {
 			Definition defCurrent = itDefinition.next();
 			if (defCurrent.getBinder() instanceof IFormulaBinder) {
-				defCurrent.getBinder().processDomino2Java(docProcess,
-						objCurrent, defCurrent.getNotesField(),
-						defCurrent.getJavaField(),
+				defCurrent.getBinder().processDomino2Java(docProcess, objCurrent, defCurrent.getNotesField(), defCurrent.getJavaField(),
 						defCurrent.getAdditionalValues());
 
 			} else {
 				if (docProcess.hasItem(defCurrent.getNotesField()) || defCurrent.getBinder().getClass().getSimpleName().equals("FileDownloadBinder")) {
-					defCurrent.getBinder().processDomino2Java(docProcess,
-							objCurrent, defCurrent.getNotesField(),
-							defCurrent.getJavaField(),
+					defCurrent.getBinder().processDomino2Java(docProcess, objCurrent, defCurrent.getNotesField(), defCurrent.getJavaField(),
 							defCurrent.getAdditionalValues());
 				}
 			}
